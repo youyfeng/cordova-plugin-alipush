70316681d4e149a76874d29a8a93f959886c7aca
llwwbb <llwwbb7@gmail.com>
2018-03-22 12:05:48 +0800

    extras统一返回为对象

--- src/android/com/blanktrack/alipush/MyMessageReceiver.java
+++ src/android/com/blanktrack/alipush/MyMessageReceiver.java
@@ -22,53 +22,62 @@ import java.util.Map;
 public class MyMessageReceiver extends MessageReceiver {
     @Override
     public void onNotification(Context context, String title, String summary, Map<String, String> extraMap) {
+        JSONObject extra = new JSONObject();
         JSONObject response = new JSONObject();
         try {
-            response.put("eventType","receiveNotification");
-            response.put("content",summary);
-            response.put("title",title);
-            response.put("extras",extraMap);
+            for (String key : extraMap.keySet()) {
+                extra.put(key, extraMap.get(key));
+            }
+            response.put("eventType", "receiveNotification");
+            response.put("content", summary);
+            response.put("title", title);
+            response.put("extras", extra);
 
             sendEvent(response);
         } catch (JSONException e) {
             sendError(e.getMessage());
         }
     }
+
     @Override
     public void onMessage(Context context, CPushMessage cPushMessage) {
         JSONObject response = new JSONObject();
         try {
-            response.put("eventType","receiveMessage");
-            response.put("messageid",cPushMessage.getMessageId());
-            response.put("title",cPushMessage.getTitle());
-            response.put("content",cPushMessage.getContent());
+            response.put("eventType", "receiveMessage");
+            response.put("messageid", cPushMessage.getMessageId());
+            response.put("title", cPushMessage.getTitle());
+            response.put("content", cPushMessage.getContent());
             sendEvent(response);
         } catch (JSONException e) {
             sendError(e.getMessage());
         }
 
     }
+
     @Override
     public void onNotificationOpened(Context context, String title, String summary, String extraMap) {
         JSONObject response = new JSONObject();
         try {
-            response.put("eventType","openNotification");
-            response.put("title",title);
-            response.put("content",summary);
-            response.put("extras",extraMap);
+            response.put("eventType", "openNotification");
+            response.put("title", title);
+            response.put("content", summary);
+            response.put("extras", new JSONObject(extraMap));
             sendEvent(response);
         } catch (JSONException e) {
             sendError(e.getMessage());
         }
     }
+
     @Override
     protected void onNotificationClickedWithNoAction(Context context, String title, String summary, String extraMap) {
         Log.e("MyMessageReceiver", "onNotificationClickedWithNoAction, title: " + title + ", summary: " + summary + ", extraMap:" + extraMap);
     }
+
     @Override
     protected void onNotificationReceivedInApp(Context context, String title, String summary, Map<String, String> extraMap, int openType, String openActivity, String openUrl) {
         Log.e("MyMessageReceiver", "onNotificationReceivedInApp, title: " + title + ", summary: " + summary + ", extraMap:" + extraMap + ", openType:" + openType + ", openActivity:" + openActivity + ", openUrl:" + openUrl);
     }
+
     @Override
     protected void onNotificationRemoved(Context context, String messageId) {
         Log.e("AliPushMessageReceiver", "onNotificationRemoved, messageId: " + messageId);
